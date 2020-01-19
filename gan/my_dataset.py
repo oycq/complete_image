6eb5e45a0b46bfbf68adeba9761ebacf5b76d6c5
oycq <490746960@qq.com>
2020-01-19 22:25:26 +0800

    ...

--- gan/my_dataset.py
+++ gan/my_dataset.py
@@ -20,6 +20,35 @@ def get_object_box_color(obeject_id):
 
 label_file_name_list = sorted(glob.glob('../../labels/train2014/*.txt'))
 
+#def get_data(index):
+#    label_file_name = label_file_name_list[index]
+#    image_file_name = label_file_name.replace('labels','images').replace('txt','jpg')
+#    image = cv2.imread(image_file_name)
+#    label_file = open(label_file_name)
+#    label_txt_lines = label_file.readlines()
+#    W,H = image.shape[1],image.shape[0]
+#    dw1 = (L-W)//2
+#    dw2 = L - W  - dw1
+#    dh1 = (L-H)//2
+#    dh2 = L - H - dh1
+#    image = cv2.copyMakeBorder(image, dh1,dh2,dw1,dw2, cv2.BORDER_CONSTANT,value=(0,0,0))
+#    image_raw = image.copy()
+#    bar = image[L // 2 - D //2 : L //2 + D//2, L // 2 - D //2 : L //2 + D//2]
+#    roi = bar.copy()
+#    bar *= 0
+##    for txt_line in label_txt_lines:
+##        txt_line = txt_line[:-2]
+##        obeject_id, x,y,w,h = txt_line.split(' ')
+##        obeject_id = int(obeject_id)
+##        x,y,w,h = float(x),float(y),float(w),float(h)
+##        x,y,w,h = int(x*W-w/2*W),int(y*H-h/2*H),int(w*W),int(h*H)
+##        x,y = x + dw1, y + dh1
+##        box_color = get_object_box_color(random.randrange(0,81))
+##        cv2.rectangle(image, (x,y), (x+w,y+h),box_color,2)
+##    cv2.imshow('image',image)
+##    cv2.imshow('roi',cv2.resize(roi, (640,640),interpolation=cv2.INTER_NEAREST))
+#    return image,image_raw, roi
+
 def get_data(index):
     label_file_name = label_file_name_list[index]
     image_file_name = label_file_name.replace('labels','images').replace('txt','jpg')
@@ -32,22 +61,13 @@ def get_data(index):
     dh1 = (L-H)//2
     dh2 = L - H - dh1
     image = cv2.copyMakeBorder(image, dh1,dh2,dw1,dw2, cv2.BORDER_CONSTANT,value=(0,0,0))
+    image = image[L // 2 - D //4 : L //2 + D//4, L // 2 - D //4 : L //2 + D//4]
     image_raw = image.copy()
-    bar = image[L // 2 - D //2 : L //2 + D//2, L // 2 - D //2 : L //2 + D//2]
+    bar = image[L // 8 - D //8 : L //8 + D//8, L // 8 - D //8 : L //8 + D//8]
     roi = bar.copy()
     bar *= 0
-#    for txt_line in label_txt_lines:
-#        txt_line = txt_line[:-2]
-#        obeject_id, x,y,w,h = txt_line.split(' ')
-#        obeject_id = int(obeject_id)
-#        x,y,w,h = float(x),float(y),float(w),float(h)
-#        x,y,w,h = int(x*W-w/2*W),int(y*H-h/2*H),int(w*W),int(h*H)
-#        x,y = x + dw1, y + dh1
-#        box_color = get_object_box_color(random.randrange(0,81))
-#        cv2.rectangle(image, (x,y), (x+w,y+h),box_color,2)
-#    cv2.imshow('image',image)
-#    cv2.imshow('roi',cv2.resize(roi, (640,640),interpolation=cv2.INTER_NEAREST))
     return image,image_raw, roi
+
 
 class MyDataset(Dataset):
     def __init__(self, train_test):
@@ -62,18 +82,18 @@ class MyDataset(Dataset):
     def __getitem__(self,index):
         if self.train_test == 'test':
             index += len(points_list) * 2 // 3
-        image, image_raw, roi =  get_data(index)
+        image, image_raw, roi =  get_data(index % 60)
         inputs = torch.tensor(image,dtype = torch.float32) / 255
         inputs = inputs.permute([2,0,1])
         ground_truth = torch.tensor(image_raw,dtype = torch.float32) / 255
         ground_truth = ground_truth.permute([2,0,1])
         roi = torch.tensor(roi,dtype = torch.float32) / 255
         roi = roi.permute([2,0,1])
-        return inputs, ground_truth
+        return inputs, roi 
 
 if __name__ == '__main__':
     for i in range(len(label_file_name_list)):
-        image,image_raw = get_data(i)
+        image,image_raw,roi = get_data(i)
         cv2.imshow('image',image)
         cv2.imshow('image_raw',image_raw)
         key = cv2.waitKey(0)
--- gan/my_model.py
+++ gan/my_model.py
@@ -3,26 +3,29 @@ import os
 import torch.nn as nn
 import cv2
 
-#640         320        160          80         40            20
-cfg_gen = [16,16,'M',32, 32, 'M', 64, 64,'M',128,128,'M',256,256, 'M', 512,512,\
-       'M',512,512,'U',512,512,'U',256,256,'U',128,128,'U',64,64,'U',32,32,'v']
-#      40          80          160         320      640 
+#     160          80         40            20          10
+cfg_gen = [32, 32, 'M', 64, 64,'M',128,128,'M',256,256, 'M', 256,'k',\
+       'U',256,256,'U',128,128,'U',64,64,'v']
+#       20          40          80                    
+#
+#     160          80         40          20             10
+cfg_judge_i = [32, 32, 'M', 64, 64,'M',128,128,'M',256,256, 'M', 512,512]
+cfg_judge_o = [64, 64,'M',128,128,'M',256,256, 'M', 512,512]
 
-#                    320        160          80         40          20
-cfg_judge = [16,16,'M',32, 32, 'M', 64, 64,'M',128,128,'M',256,256, 'M', 512,512,'M',512,512]
 
 
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
         self.gen_cnn = self._make_layers(cfg_gen, batch_norm=True, in_channels = 3)
-        self.judge_cnn = self._make_layers(cfg_judge, batch_norm=True, in_channels = 3)
+        self.judge_cnn_i = self._make_layers(cfg_judge_i, batch_norm=True, in_channels=3)
+        self.judge_cnn_o = self._make_layers(cfg_judge_o, batch_norm=True, in_channels=3)
         self.judge_score = nn.Sequential(
-            nn.Linear(10*10*512, 256),
-            nn.BatchNorm1d(256),
+            nn.Linear(10*10*512*2, 256),
+            #nn.BatchNorm1d(256),
             nn.ReLU(inplace = True),
             nn.Linear(256, 256),
-            nn.BatchNorm1d(256),
+            #nn.BatchNorm1d(256),
             nn.ReLU(inplace = True),
             nn.Linear(256, 1),
             nn.Sigmoid()
@@ -39,32 +42,30 @@ class Model(nn.Module):
             a = [False,True,True]
         for param in self.gen_cnn.parameters():
             param.requires_grad = a[0]
-        for param in self.judge_cnn.parameters():
+        for param in self.judge_cnn_i.parameters():
+            param.requires_grad = a[1]
+        for param in self.judge_cnn_o.parameters():
             param.requires_grad = a[1]
         for param in self.judge_score.parameters():
             param.requires_grad = a[2]
         
 
-    def gen(self, inputs):
-        self.open_grad('gen')
+    def forward(self, inputs, groud_truth,grad='gen'):
+        self.open_grad(grad)
         gen_image = self.gen_cnn(inputs)
-        gen_image += inputs
-        x = self.judge_cnn(gen_image)
+        x = self.judge_cnn_i(inputs)
         x = x.flatten(1) 
-        conf_for_gen = self.judge_score(x)
-        return gen_image,conf_for_gen
-
-    def judge(self, real_image, inputs):
-        self.open_grad('judge')
-        fake_image = self.gen_cnn(inputs)
-        fake_image += inputs
-        x = self.judge_cnn(real_image)
-        x = x.flatten(1)
-        conf_for_real = self.judge_score(x)
-        x = self.judge_cnn(fake_image)
+        y = self.judge_cnn_o(gen_image)
+        y = y.flatten(1) 
+        z = torch.cat([x,y],1)
+        conf_for_gen = self.judge_score(z)
+        x = self.judge_cnn_i(inputs)
         x = x.flatten(1) 
-        conf_for_fake = self.judge_score(x)
-        return conf_for_fake, conf_for_real, fake_image
+        y = self.judge_cnn_o(groud_truth)
+        y = y.flatten(1) 
+        z = torch.cat([x,y],1)
+        conf_for_real = self.judge_score(z)
+        return gen_image, conf_for_gen, conf_for_real
 
     def _make_layers(self,cfg,batch_norm,in_channels = 3):
         layers = []
@@ -72,11 +73,17 @@ class Model(nn.Module):
         for v in cfg:
             if v == 'v':
                 conv2d = nn.Conv2d(in_channels, 3, kernel_size=3, padding=1, bias=False)
-                #layers += [conv2d, nn.BatchNorm2d(3), nn.Sigmoid()]
-                layers += [conv2d, nn.Sigmoid()]
-                layers += [nn.ZeroPad2d(160)]
+                layers += [conv2d, nn.BatchNorm2d(3)]
+                #conv2d = nn.Conv2d(3, 3, kernel_size=3, padding=1, bias=False)
+                #layers += [conv2d, nn.ReLU()]
+                #layers += [conv2d]
+                continue
+            if v == 'k':
+                conv2d = nn.Conv2d(256, 256, kernel_size=5, padding=2, bias=False)
+                layers += [conv2d, nn.BatchNorm2d(256), nn.ReLU()]
                 continue
             if v == 'M':
+                #layers += [nn.AvgPool2d(kernel_size=2, stride=2)]
                 layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
                 continue
             if v == 'U':
